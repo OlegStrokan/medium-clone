e9cf747ef5ac1c4164b8656cefea8ff7caa7f0a2
Oleh Strokan <oleh.strokan@enrian.com>
2022-10-23 18:38:13 +0200

    v 0.0.5. Fix mailer controller

--- mailer/Controllers/MailerController.cs
+++ mailer/Controllers/MailerController.cs
@@ -1,33 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
 
 namespace mailer.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+[Route("/")]
+public class MailController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly ILogger<MailController> _logger;
+    private readonly IMailerService _service;
+    public MailController(ILogger<MailController> logger, IMailerService service)
     {
         _logger = logger;
+        _service = service;
     }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    
+    public async Task<EmailSendResponseDto> SendEmail(EmailDataDto dto)
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return await _service.MailSend(dto);
     }
 }
 
