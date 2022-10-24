76988d537e85955aa02221623cba506a7263be61
Oleh Strokan <oleg14ua71@gmail.com>
2022-10-24 23:06:25 +0200

    v 0.1.3. Added database config

--- user/src/user.module.ts
+++ user/src/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './services/user.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {dbConfigService} from "./services/db-config.service";
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot()
+      TypeOrmModule.forRoot(dbConfigService.getTypeOrmConfig())
   ],
   controllers: [UserController],
   providers: [UserService],
