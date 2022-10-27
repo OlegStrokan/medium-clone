43ea0a79f5a170fb590b7667d6c1b6c32bd8fe05
Oleh Strokan <oleg14ua71@gmail.com>
2022-10-27 16:22:09 +0200

    v 0.5.2. Added token entity to token service

--- token/src/main.ts
+++ token/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { TokenModule } from './token.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(TokenModule);
   await app.listen(3000);
 }
 bootstrap();
