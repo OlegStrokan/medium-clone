9a9b5eadf7214e97565a13ec9127a02514c2db97
oleh14ua71@gmail.com <oleg14ua71@gmail.com>
2023-03-17 09:55:04 +0100

    v1.0.7. Added helpers method to user service

---
+++ user/src/services/user.service.ts
@@ -0,0 +1,68 @@
+import {HttpStatus, Injectable} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import {IUser} from "../interfaces/IUser";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {UserEntity} from "../repository/user.entity";
+import {UserResponseDto} from "../interfaces/response-dtos/user-response.dto";
+import {MessageEnums} from "../interfaces/message-enums/message.enums";
+import {CreateUserDto} from "../interfaces/request-dtos/create-user.dto";
+
+Injectable()
+export class UserService {
+  constructor(
+      @InjectRepository(UserEntity)
+      private readonly userRepository: Repository<IUser>,
+  ) {}
+
+  async getUser(id: string): Promise<UserResponseDto> {
+    const user =  await this.getUserById(id);
+    if (!user) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: MessageEnums.NOT_FOUND,
+        data: null,
+      }
+    } else {
+      return {
+        status: HttpStatus.OK,
+        message: MessageEnums.OK,
+        data: user
+      }
+    }
+  }
+
+
+  async updateUser(id: number, userData: Partial<IUser>): Promise<IUser> {
+    const user = await this.userRepository.findOneOrFail(id);
+    Object.assign(user, userData);
+    return this.userRepository.save(user);
+  }
+
+  async deleteUser(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+
+
+  private async getUserById(id: string): Promise<IUser>  {
+    return this.userRepository.findOneBy({ id })
+  }
+
+  private async getUserByEmail(email: string): Promise<IUser>  {
+    return this.userRepository.findOneBy({ email })
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt);
+  }
+
+  private async validateUser(email: string, password: string): Promise<IUser> {
+    const user = await this.getUserByEmail(email);
+
+    if  (user && await bcrypt.compare(password, user.password)) {
+      return user
+    }
+    return null
+  }
+}
