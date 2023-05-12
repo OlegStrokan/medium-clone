5a46ef7a89f1506be03044f6c5935060273c13cd
oleh14ua71@gmail.com <oleg14ua71@gmail.com>
2023-05-12 12:22:52 +0200

    v1.2.8. Updated dtos, exception service

--- gateway/src/interfaces/user/dto/create-user.dto.ts
+++ gateway/src/interfaces/user/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
-export interface CreateUserDto {
+import {IsEmail, IsNotEmpty, IsString, Length} from "class-validator";
+import {Transform} from "class-transformer";
+
+
+export class CreateUserDto {
+    // TODO - update validation
+    @IsString({ message: 'Full name must be a string' })
+    @IsNotEmpty({ message: 'Full name can\'t be empty'})
     fullName: string;
+    @IsString({ message: 'User name must be a string' })
+    @IsNotEmpty({ message: 'User name can\'t be empty'})
     userName: string;
+    @IsString({ message: 'Email name must be a string' })
+    @IsNotEmpty({ message: 'Email name can\'t be empty'})
+    @IsEmail()
+    @Transform(({value}) => value.toLowerCase())
     email: string;
+    @IsString({ message: 'Password name must be a string' })
+    @IsNotEmpty({ message: 'Password name can\'t be empty'})
+    @Length(6,20, {message: "Password must be at least 6 characters long"})
+    @Transform(({ value }) => value.trim())
     password: string;
 }
