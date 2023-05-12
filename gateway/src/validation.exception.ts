d39f0111dc6db632c39d7a68244ed7b9b0198b43
oleh14ua71@gmail.com <oleg14ua71@gmail.com>
2023-05-12 12:22:22 +0200

    v1.2.7. Added custom validation

---
+++ gateway/src/validation.exception.ts
@@ -0,0 +1,11 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+
+export class ValidationException extends HttpException {
+    messages;
+
+    constructor(response) {
+        super(response, HttpStatus.BAD_REQUEST);
+        this.messages = response;
+    }
+}
---
+++ gateway/src/validation.pipe.ts
@@ -0,0 +1,27 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import {ValidationException} from "./validation.exception";
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+    async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
+        const obj = plainToInstance(metadata.metatype, value);
+        const errors = await validate(obj);
+
+        if (errors.length) {
+            let messages = errors.map(err => {
+                return Object.values(err.constraints).join(', ')
+            })
+
+            throw new ValidationException({
+                data: null,
+                errors: {
+                    message: messages
+                }
+            })
+        }
+        return value;
+    }
+
+}
