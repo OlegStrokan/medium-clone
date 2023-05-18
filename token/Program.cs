62cc9066c322df69636f6837e232b774b763490a
Oleh Strokan <oleh.strokan@enrian.com>
2023-05-18 12:51:11 +0200

    v1.5.1. Updated packages

--- token/Program.cs
+++ token/Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using token.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
