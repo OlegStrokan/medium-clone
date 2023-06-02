78980f2b31c811fbe68f145f8914a997469c5455
Oleh Strokan <oleh.strokan@enrian.com>
2023-06-02 21:49:21 +0200

    v1.8.5. Updated rabbitmq interface

--- token/TokenService/Services/RabbitMQService/IRabbitMqService.cs
+++ token/TokenService/Services/RabbitMQService/IRabbitMqService.cs
@@ -1,8 +1,8 @@
 
 namespace TokenService.Services.RabbitMQService;
 
-public interface IRabbitMqService : IDisposable
+public interface IRabbitMqService
 {
-    void PublishMessage(string message);
-    void StartListening(Action<string> onMessageReceived);
+    void PublishMessage(string queueName, string message);
+    void StartListening(string queueName, Action<string> onMessageReceived);
 }
