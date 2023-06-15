4963234c2117409860a65894047e9d705e92ceb6
Oleh Strokan <oleh.strokan@enrian.com>
2023-06-15 13:11:49 +0200

    v2.6.1. Added logs to user microservice, updated user tests

--- user/src/services/user.service.spec.ts
+++ user/src/services/user.service.spec.ts
@@ -6,17 +6,19 @@ import {UserResponseDto} from "../interfaces/response-dtos/user-response.dto";
 import {HttpStatus} from "@nestjs/common";
 import {MessageEnum} from "../interfaces/message-enums/message.enum";
 import {UpdateUserDto} from "../interfaces/request-dtos/update-user.dto";
+import {IUser} from "../interfaces/IUser";
 
 describe('UserService', () => {
     let userService: UserService
     let userRepository: UserEntity
 
-    const testUser = {
+    const testUser: IUser = {
         id: '20392039',
         email: 'test@example.com',
         password: 'pas099sword',
         fullName: "Oleh Strokan",
         userName: "stroka01",
+        activationLink: { link: 'https://google.com'}
     }
 
 
@@ -49,20 +51,20 @@ describe('UserService', () => {
 
             jest.spyOn(userService as any, 'searchUserHelper').mockResolvedValue(testUser)
 
-            const result: UserResponseDto = await userService.getUser(testUser.id)
+            const result: UserResponseDto<IUser> = await userService.getUser(testUser.id)
 
             expect(result.status).toEqual(HttpStatus.OK)
-            expect(result.message).toEqual(MessageEnum.OK)
+            expect(result.message).toEqual(MessageEnum.USER_SEARCH_OK)
             expect(result.data).toEqual(testUser)
         });
         it('should return 404 not found when user does not exist', async () => {
 
             jest.spyOn(userService as any, 'searchUserHelper').mockResolvedValue(null);
 
-            const result: UserResponseDto = await userService.getUser(testUser.id);
+            const result: UserResponseDto<IUser> = await userService.getUser(testUser.id);
 
             expect(result.status).toEqual(HttpStatus.NOT_FOUND)
-            expect(result.message).toEqual(MessageEnum.NOT_FOUND)
+            expect(result.message).toEqual(MessageEnum.USER_NOT_FOUND_ID)
             expect(result.data).toBeNull()
         });
     })
@@ -80,10 +82,10 @@ describe('UserService', () => {
             jest.spyOn(userService.userRepository, 'create').mockReturnValue(testUser)
             jest.spyOn(userService.userRepository, 'save').mockResolvedValue(testUser)
 
-            const result: UserResponseDto = await userService.createUser(user)
+            const result: UserResponseDto<IUser> = await userService.createUser(user)
 
             expect(result.status).toEqual(HttpStatus.CREATED)
-            expect(result.message).toEqual(MessageEnum.CREATED)
+            expect(result.message).toEqual(MessageEnum.USER_CREATED)
             expect(result.data.email).toBe(testUser.email)
         });
     })
@@ -97,18 +99,22 @@ describe('UserService', () => {
             userName: 'User',
         };
 
-        const existingUser = {
+        const existingUser: IUser = {
             id: '1',
             email: 'old@test.com',
             fullName: 'Olejandro Stroka',
             userName: 'stroka01',
+            activationLink: { link: 'https://google.com'}
+
         }
 
-        const updatedUser = {
+        const updatedUser: IUser = {
             id: '1',
             email: 'test@test.com',
             fullName: 'Oleh Strokan',
             userName: 'stroka02',
+            activationLink: { link: 'https://google.com'}
+
         };
 
         it('should update a user if user already exist', async () => {
@@ -120,7 +126,7 @@ describe('UserService', () => {
 
             expect(result).toEqual({
                 status: HttpStatus.OK,
-                message: MessageEnum.OK,
+                message: MessageEnum.USER_UPDATED,
                 data: updatedUser,
             });
         })
@@ -132,18 +138,20 @@ describe('UserService', () => {
 
             expect(result).toEqual({
                 status: HttpStatus.NOT_FOUND,
-                message: MessageEnum.NOT_FOUND,
+                message: MessageEnum.USER_NOT_FOUND_ID,
                 data: null
             })
         });
     })
 
     describe('deleteUser', () => {
-        const existingUser = {
+        const existingUser: IUser = {
             id: '1',
             email: 'old@test.com',
             fullName: 'Olejandro Stroka',
             userName: 'stroka01',
+            activationLink: { link: 'https://google.com'}
+
         }
 
         it('should delete a user and return NO_CONTENT response', async () => {
@@ -152,14 +160,14 @@ describe('UserService', () => {
             userRepository.delete = jest.fn().mockResolvedValue(undefined);
 
 
-            const result: UserResponseDto = await userService.deleteUser(existingUser.id);
+            const result: UserResponseDto<IUser> = await userService.deleteUser(existingUser.id);
 
 
             expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: existingUser.id });
             expect(userRepository.delete).toHaveBeenCalledWith(existingUser.id);
             expect(result).toEqual({
                 status: HttpStatus.NO_CONTENT,
-                message: MessageEnum.NO_CONTENT,
+                message: MessageEnum.USER_DELETED,
                 data: null,
             });
         });
@@ -169,13 +177,13 @@ describe('UserService', () => {
             userRepository.findOneBy = jest.fn().mockRejectedValue(undefined);
 
 
-            const result: UserResponseDto = await userService.deleteUser(existingUser.id);
+            const result: UserResponseDto<IUser> = await userService.deleteUser(existingUser.id);
 
 
             expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: existingUser.id });
             expect(result).toEqual({
                 status: HttpStatus.NOT_FOUND,
-                message: MessageEnum.NOT_FOUND,
+                message: MessageEnum.USER_NOT_FOUND_ID,
                 data: 'Test for hooks',
             });
         });
@@ -186,7 +194,7 @@ describe('UserService', () => {
             userRepository.delete = jest.fn().mockRejectedValue(undefined);
 
 
-            const result: UserResponseDto = await userService.deleteUser(existingUser.id);
+            const result: UserResponseDto<IUser> = await userService.deleteUser(existingUser.id);
 
 
             expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: existingUser.id });
--- user/src/services/user.service.ts
+++ user/src/services/user.service.ts
@@ -1,54 +1,61 @@
-import {HttpStatus, Inject, Injectable} from '@nestjs/common';
+import {HttpStatus, Inject, Injectable, Logger} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import {IUser} from "../interfaces/IUser";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
-import {UserEntity} from "../repository/user.entity";
-import {UserResponseDto} from "../interfaces/response-dtos/user-response.dto";
-import {MessageEnum} from "../interfaces/message-enums/message.enum";
-import {CreateUserDto} from "../interfaces/request-dtos/create-user.dto";
-import {UpdateUserDto} from "../interfaces/request-dtos/update-user.dto";
-import {ValidateUserDto} from "../interfaces/request-dtos/validate-user.dto";
-import * as uuid from 'uuid'
-import {ActivationLinkEntity} from "../repository/activationLink.entity";
-import {IActivationLink} from "../interfaces/IActivationLink";
-import {ClientProxy} from "@nestjs/microservices";
-import {firstValueFrom} from "rxjs";
-import {MessageRoleEnum} from "../interfaces/message-enums/message-role";
-import {IRole} from "../interfaces/IRole";
-import {MessageUserRoleEnum} from "../interfaces/message-enums/message-user-role.enum";
+import {IUser} from '../interfaces/IUser';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+import {UserEntity} from '../repository/user.entity';
+import {UserResponseDto} from '../interfaces/response-dtos/user-response.dto';
+import {MessageEnum} from '../interfaces/message-enums/message.enum';
+import {CreateUserDto} from '../interfaces/request-dtos/create-user.dto';
+import {UpdateUserDto} from '../interfaces/request-dtos/update-user.dto';
+import {ValidateUserDto} from '../interfaces/request-dtos/validate-user.dto';
+import * as uuid from 'uuid';
+import {ActivationLinkEntity} from '../repository/activationLink.entity';
+import {IActivationLink} from '../interfaces/IActivationLink';
+import {ClientProxy} from '@nestjs/microservices';
+import {firstValueFrom} from 'rxjs';
+import {MessageRoleEnum} from '../interfaces/message-enums/message-role';
+import {IRole} from '../interfaces/IRole';
+import {MessageUserRoleEnum} from '../interfaces/message-enums/message-user-role.enum';
+import {UserLogsEnum} from '../interfaces/message-enums/user-logs.enum';
 
-Injectable()
-
+@Injectable()
 export class UserService {
+    private readonly logger: Logger;
+
     constructor(
         @InjectRepository(UserEntity)
         public readonly userRepository: Repository<IUser>,
         @InjectRepository(ActivationLinkEntity)
         public readonly activationLinkRepository: Repository<IActivationLink>,
         @Inject('role_service') private readonly roleService: ClientProxy,
-        @Inject('user_role_service') private readonly userRoleService: ClientProxy
+        @Inject('user_role_service') private readonly userRoleService: ClientProxy,
     ) {
+        this.logger = new Logger(UserService.name);
     }
 
     public async getUser(id: string): Promise<UserResponseDto<IUser>> {
+        this.logger.log(UserLogsEnum.USER_INITIATED);
 
         try {
             const user = await this.searchUserHelper(id, 'id');
             if (!user) {
+                this.logger.log(UserLogsEnum.USER_NOT_FOUND);
                 return {
                     status: HttpStatus.NOT_FOUND,
                     message: MessageEnum.USER_NOT_FOUND_ID,
                     data: null,
-                }
+                };
             }
 
+            this.logger.log(UserLogsEnum.USER_RETRIEVED_SUCCESS);
             return {
                 status: HttpStatus.OK,
                 message: MessageEnum.USER_SEARCH_OK,
-                data: user
-            }
+                data: user,
+            };
         } catch (e) {
+            this.logger.log(UserLogsEnum.USER_RETRIEVING_ERROR);
             return {
                 status: HttpStatus.PRECONDITION_FAILED,
                 message: MessageEnum.PRECONDITION_FAILED,
@@ -59,222 +66,248 @@ export class UserService {
     }
 
     public async getUserByEmail(email: string): Promise<UserResponseDto<IUser>> {
+        this.logger.log(UserLogsEnum.USERS_INITIATED);
 
         try {
             const user = await this.searchUserHelper(email, 'email');
             if (!user) {
+                this.logger.log(UserLogsEnum.USER_NOT_FOUND);
                 return {
                     status: HttpStatus.NOT_FOUND,
                     message: MessageEnum.USER_NOT_FOUND_EMAIL,
                     data: null,
-                }
+                };
             }
 
+            this.logger.log(UserLogsEnum.USER_RETRIEVED_SUCCESS);
             return {
                 status: HttpStatus.OK,
                 message: MessageEnum.USER_SEARCH_OK,
-                data: user
-            }
+                data: user,
+            };
         } catch (e) {
+            this.logger.log(UserLogsEnum.USER_RETRIEVING_ERROR);
             return {
                 status: HttpStatus.PRECONDITION_FAILED,
                 message: MessageEnum.PRECONDITION_FAILED,
                 data: null,
                 errors: e
-            };
+            }
         }
     }
 
     public async validateUser(dto: ValidateUserDto): Promise<UserResponseDto<IUser>> {
+        this.logger.log(UserLogsEnum.USER_VALIDATION_INITIATED);
 
         try {
-
             const user = await this.searchUserHelper(dto.email, 'email');
 
             if (!user) {
+                this.logger.log(UserLogsEnum.USER_NOT_FOUND);
                 return {
                     status: HttpStatus.NOT_FOUND,
                     message: MessageEnum.USER_NOT_FOUND_ID,
-                    data: null
-                }
+                    data: null,
+                };
             }
 
-            if (user && await bcrypt.compare(dto.password, user.password)) {
+            if (user && (await bcrypt.compare(dto.password, user.password))) {
+                this.logger.log(UserLogsEnum.USER_VALIDATION_SUCCESS);
                 return {
                     status: HttpStatus.OK,
                     message: MessageEnum.USER_SEARCH_OK,
-                    data: user
-                }
+                    data: user,
+                };
             } else {
+                this.logger.log(UserLogsEnum.USER_VALIDATION_FAILED);
                 return {
                     status: HttpStatus.UNAUTHORIZED,
                     message: MessageEnum.UNAUTHORIZED,
-                    data: null
-                }
+                    data: null,
+                };
             }
         } catch (e) {
+            this.logger.log(UserLogsEnum.USER_VALIDATION_ERROR);
             return {
                 status: HttpStatus.PRECONDITION_FAILED,
                 message: MessageEnum.PRECONDITION_FAILED,
                 data: null,
-            }
+            };
         }
-
     }
 
     public async getUsers(): Promise<UserResponseDto<IUser[]>> {
+        this.logger.log(UserLogsEnum.USERS_INITIATED);
 
         try {
             const users = await this.userRepository.find();
 
+            this.logger.log(UserLogsEnum.USERS_RETRIEVED_SUCCESS);
             return {
                 status: HttpStatus.OK,
                 message: MessageEnum.USER_SEARCH_OK,
-                data: users
-            }
+                data: users,
+            };
         } catch (e) {
+            this.logger.log(UserLogsEnum.USERS_RETRIEVING_ERROR);
             return {
                 status: HttpStatus.PRECONDITION_FAILED,
                 message: MessageEnum.PRECONDITION_FAILED,
-                data: null
-            }
+                data: null,
+            };
         }
     }
 
     public async createUser(dto: CreateUserDto): Promise<UserResponseDto<IUser>> {
-        const existingUser = await this.userRepository.findOne({where: {email: dto.email}});
+        this.logger.log(UserLogsEnum.USER_CREATION_INITIATED);
+
+        const existingUser = await this.userRepository.findOne({
+            where: {email: dto.email},
+        });
 
         if (existingUser) {
+            this.logger.log(UserLogsEnum.USER_CREATION_CONFLICT);
             return {
                 status: HttpStatus.CONFLICT,
                 message: MessageEnum.USER_CONFLICT,
                 data: null,
                 errors: {
-                    messages: [MessageEnum.USER_CONFLICT]
-                }
-            }
+                    messages: [MessageEnum.USER_CONFLICT],
+                },
+            };
         }
-        try {
-            const hashPassword = await UserService.hashPassword(dto.password)
 
-            const user = await this.userRepository.create({...dto, password: hashPassword,});
+        try {
+            const hashPassword = await UserService.hashPassword(dto.password);
+
+            const user = await this.userRepository.create({
+                ...dto,
+                password: hashPassword,
+            });
             await this.userRepository.save(user);
 
-            const activationLink = await this.activationLinkRepository.create({userId: user.id, link: uuid.v4()})
+            const activationLink = await this.activationLinkRepository.create({
+                userId: user.id,
+                link: uuid.v4(),
+            });
 
             await this.activationLinkRepository.save(activationLink);
 
             const roleServiceResponse: UserResponseDto<IRole> = await firstValueFrom(
-                this.roleService.send(MessageRoleEnum.ROLE_GET_BY_VALUE, "admin")
-            )
+                this.roleService.send(MessageRoleEnum.ROLE_GET_BY_VALUE, 'admin'),
+            );
 
-           await firstValueFrom(this.userRoleService.send(
-               MessageUserRoleEnum.ROLE_ASSIGN_TO_USER, JSON.stringify({ userId: user.id, roleId: roleServiceResponse.data.id }))
-           )
+            await firstValueFrom(
+                this.userRoleService.send(
+                    MessageUserRoleEnum.ROLE_ASSIGN_TO_USER,
+                    JSON.stringify({userId: user.id, roleId: roleServiceResponse.data.id}),
+                ),
+            );
 
-
-
-
-            const response = await this.userRepository.createQueryBuilder('user')
+            const response = await this.userRepository
+                .createQueryBuilder('user')
                 .leftJoinAndSelect('user.activationLink', 'activationLink')
                 .select([
                     'user.id',
                     'user.fullName',
                     'user.userName',
                     'user.email',
-                    'activationLink.link'
+                    'activationLink.link',
                 ])
                 .getOne();
 
-
-
+            this.logger.log(UserLogsEnum.USER_CREATED);
             return {
                 status: HttpStatus.CREATED,
                 message: MessageEnum.USER_CREATED,
-                data: {
-                    ...response,
-                    roles: [roleServiceResponse.data.value]
-                }
-            }
-
-        } catch (error) {
+                data: response,
+            };
+        } catch (e) {
+            this.logger.log(UserLogsEnum.USER_CREATION_ERROR);
             return {
                 status: HttpStatus.PRECONDITION_FAILED,
                 message: MessageEnum.PRECONDITION_FAILED,
                 data: null,
-            }
+                errors: e,
+            };
         }
-
     }
 
     public async updateUser(dto: UpdateUserDto): Promise<UserResponseDto<IUser>> {
+        this.logger.log(UserLogsEnum.USER_UPDATE_INITIATED);
 
-        const user = await this.searchUserHelper(dto.id, 'id');
-
-        if (!user) {
-            return {
-                status: HttpStatus.NOT_FOUND,
-                message: MessageEnum.USER_NOT_FOUND_ID,
-                data: null,
-            }
-        } else {
-            try {
-                Object.assign(user, dto);
-                const updatedUser = await this.userRepository.save(user);
-
-                return {
-                    status: HttpStatus.OK,
-                    message: MessageEnum.USER_UPDATED,
-                    data: updatedUser
-                }
-
-            } catch (e) {
-                return {
-                    status: HttpStatus.PRECONDITION_FAILED,
-                    message: MessageEnum.PRECONDITION_FAILED,
-                    data: null,
-                }
-            }
-        }
-    }
-
-    public async deleteUser(id: string): Promise<UserResponseDto<null>> {
         try {
-
-            const user = this.searchUserHelper(id, 'id')
-
+            const user = await this.searchUserHelper(dto.id, 'id');
             if (!user) {
+                this.logger.log(UserLogsEnum.USER_NOT_FOUND);
                 return {
                     status: HttpStatus.NOT_FOUND,
                     message: MessageEnum.USER_NOT_FOUND_ID,
                     data: null,
-                }
+                };
             }
 
-            await this.userRepository.delete(id);
+            await this.userRepository.update(dto.id, dto);
 
+            const updatedUser = await this.searchUserHelper(dto.id, 'id');
+
+            this.logger.log(UserLogsEnum.USER_UPDATED_SUCCESS);
             return {
-                status: HttpStatus.NO_CONTENT,
-                message: MessageEnum.USER_DELETED,
-                data: null
-            }
-
-
+                status: HttpStatus.OK,
+                message: MessageEnum.USER_UPDATED,
+                data: updatedUser,
+            };
         } catch (e) {
+            this.logger.log(UserLogsEnum.USER_UPDATE_ERROR);
             return {
                 status: HttpStatus.PRECONDITION_FAILED,
                 message: MessageEnum.PRECONDITION_FAILED,
                 data: null,
-            }
+                errors: e,
+            };
         }
     }
 
-    private async searchUserHelper(dto, value): Promise<IUser> {
-        return await this.userRepository.findOneBy({[value]: dto})
+    public async deleteUser(id: string): Promise<UserResponseDto<IUser>> {
+        this.logger.log(UserLogsEnum.USER_DELETION_INITIATED);
+
+        try {
+            const user = await this.searchUserHelper(id, 'id');
+            if (!user) {
+                this.logger.log(UserLogsEnum.USER_NOT_FOUND);
+                return {
+                    status: HttpStatus.NOT_FOUND,
+                    message: MessageEnum.USER_NOT_FOUND_ID,
+                    data: null,
+                };
+            }
+
+            await this.userRepository.delete(id);
+
+            this.logger.log(UserLogsEnum.USER_DELETED);
+            return {
+                status: HttpStatus.OK,
+                message: MessageEnum.USER_DELETED,
+                data: null,
+            };
+        } catch (e) {
+            this.logger.log(UserLogsEnum.USER_DELETION_ERROR);
+            return {
+                status: HttpStatus.PRECONDITION_FAILED,
+                message: MessageEnum.PRECONDITION_FAILED,
+                data: null,
+                errors: e,
+            };
+        }
+    }
+
+    private async searchUserHelper(value: string, field: string): Promise<IUser | undefined> {
+        return this.userRepository.findOne({where: {[field]: value}});
     }
 
     private static async hashPassword(password: string): Promise<string> {
-        const salt = await bcrypt.genSalt(10);
-        return await bcrypt.hash(password, salt);
+        const saltRounds = 10;
+        return bcrypt.hash(password, saltRounds);
     }
 }
+
