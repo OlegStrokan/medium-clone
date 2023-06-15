5f3baec231d5f8e67a0d68138f08b94db7774979
Oleh Strokan <oleh.strokan@enrian.com>
2023-06-15 12:41:29 +0200

    v2.6.0. Added logs to user controller

--- gateway/src/controllers/users.controller.ts
+++ gateway/src/controllers/users.controller.ts
@@ -3,7 +3,7 @@ import {
     Controller, Delete,
     Get,
     HttpStatus,
-    Inject,
+    Inject, Logger,
     Param,
     Patch,
     Post, UseGuards
@@ -20,44 +20,59 @@ import {IError} from "../interfaces/IError";
 import {UpdateUserDto} from "../interfaces/user/dto/update-user.dto";
 import {DeleteUserDto} from "../interfaces/user/dto/delete-user.dto";
 import {AuthGuard} from "../helpers/auth.guard";
+import {UserLogsEnum} from "../interfaces/user/user-logs.enum";
 
 
 @Controller('users')
 export class UsersController {
+    private readonly logger: Logger;
     constructor(
-        @Inject('user_service') private readonly userServiceClient: ClientProxy
+        @Inject('user_service') private readonly userServiceClient: ClientProxy,
     ) {
+        this.logger = new Logger(UsersController.name)
+
     }
 
     @Get('/:id')
     public async getUser(@Param('id') id: number): Promise<IGetItemResponse<IUser> | GenericHttpException> {
+
+        this.logger.log(UserLogsEnum.USER_INITIATED)
+
         const userResponse: IGetItemServiceResponse<IUser> = await firstValueFrom(
             this.userServiceClient.send(MessageUserEnum.USER_GET_BY_ID, id)
         )
         if (userResponse.status === HttpStatus.OK) {
+            this.logger.log(UserLogsEnum.USER_RETRIEVED_SUCCESS)
             return {
                 data: userResponse.data,
                 status: userResponse.status,
             }
         } else if (userResponse.status === HttpStatus.NOT_FOUND) {
+            this.logger.log(UserLogsEnum.USER_NOT_FOUND)
             throw new GenericHttpException<IError>(404, userResponse.message)
         } else {
+            this.logger.log(UserLogsEnum.USER_RETRIEVING_ERROR)
             throw new GenericHttpException<IError>(412, userResponse.message)
         }
     }
 
     @Get("")
     public async getUsers(): Promise<IGetItemResponse<IUser[]> | GenericHttpException> {
+
+        this.logger.log(UserLogsEnum.USERS_INITIATED)
+
         const userResponse: IGetItemServiceResponse<IUser[]> = await firstValueFrom(
             this.userServiceClient.send(MessageUserEnum.USER_GET, 'test')
         )
 
         if (userResponse.status === HttpStatus.OK) {
+            this.logger.log(UserLogsEnum.USERS_RETRIEVED_SUCCESS)
             return {
                 data: userResponse.data,
                 status: userResponse.status
             }
         } else {
+            this.logger.log(UserLogsEnum.USERS_RETRIEVING_ERROR)
             throw new GenericHttpException<IError>(412, userResponse.message)
         }
 
@@ -65,18 +80,25 @@ export class UsersController {
 
     @Post("")
     public async createUser(@Body() dto: CreateUserDto): Promise<IGetItemResponse<IUser> | GenericHttpException> {
+
+
+        this.logger.log(UserLogsEnum.USER_CREATE_INITIATED)
+
         const userResponse: IGetItemServiceResponse<IUser> = await firstValueFrom(
             this.userServiceClient.send(MessageUserEnum.USER_CREATE, JSON.stringify(dto))
         )
 
         if (userResponse.status === HttpStatus.CREATED) {
+            this.logger.log(UserLogsEnum.USER_CREATED_SUCCESS)
             return {
                 data: userResponse.data,
                 status: userResponse.status
             }
         } else if (userResponse.status === HttpStatus.CONFLICT) {
+            this.logger.log(UserLogsEnum.USER_CREATED_CONFLICT)
             throw new GenericHttpException<IError>(409, userResponse.message)
         } else {
+            this.logger.log(UserLogsEnum.USER_CREATED_ERROR)
             throw new GenericHttpException<IError>(412, userResponse.message)
         }
     }
@@ -85,35 +107,46 @@ export class UsersController {
     @Patch("/:id")
     public async updateUser(@Body() dto: UpdateUserDto, @Param('id') id: DeleteUserDto): Promise<IGetItemResponse<IUser> | GenericHttpException> {
 
+        this.logger.log(UserLogsEnum.USER_UPDATE_INITIATED)
+
         const userResponse: IGetItemServiceResponse<IUser> = await firstValueFrom(
             this.userServiceClient.send(MessageUserEnum.USER_UPDATE, JSON.stringify({id, ...dto}))
         )
 
         if (userResponse.status === HttpStatus.OK) {
+            this.logger.log(UserLogsEnum.USER_UPDATED_SUCCESS)
             return {
                 data: userResponse.data,
                 status: userResponse.status
             }
         } else if (userResponse.status === HttpStatus.NOT_FOUND) {
+            this.logger.log(UserLogsEnum.USER_UPDATED_NOT_FOUND)
             throw new GenericHttpException<IError>(404, userResponse.message)
         } else {
+            this.logger.log(UserLogsEnum.USER_UPDATED_FAILED)
             throw new GenericHttpException<IError>(412, userResponse.message)
         }
     }
 
     @Delete('/:id')
     public async deleteUser(@Param('id') id: DeleteUserDto): Promise<IGetItemResponse<string> | GenericHttpException> {
+
+        this.logger.log(UserLogsEnum.USER_DELETED_INITIATED)
+
         const userResponse: IGetItemServiceResponse<IUser> = await firstValueFrom(
             this.userServiceClient.send(MessageUserEnum.USER_DELETE, id)
         )
 
         if (userResponse.status === HttpStatus.NO_CONTENT) {
+            this.logger.log(UserLogsEnum.USER_DELETED_SUCCESS)
             return {
                 status: userResponse.status
             }
         } else if (userResponse.status === HttpStatus.NOT_FOUND) {
+            this.logger.log(UserLogsEnum.USER_DELETED_NOT_FOUND)
             throw new GenericHttpException<IError>(404, userResponse.message)
         } else {
+            this.logger.log(UserLogsEnum.USER_DELETED_ERROR)
             throw new GenericHttpException<IError>(412, userResponse.message)
         }
 
--- gateway/src/interfaces/user/user-logs.enum.ts
+++ gateway/src/interfaces/user/user-logs.enum.ts
@@ -2,15 +2,25 @@ export enum UserLogsEnum {
     USER_INITIATED = 'Get user process initiated',
     USER_RETRIEVED_SUCCESS = 'User retrieved success',
     USER_NOT_FOUND = 'User not found',
-    ERROR_RETRIEVING_USER = 'Error retrieved user',
+    USER_RETRIEVING_ERROR = 'Error retrieved user',
 
     USERS_INITIATED = 'Get users process initiated',
     USERS_RETRIEVED_SUCCESS = 'Users retrieved success',
-    ERROR_RETRIEVING_USERS = 'Error retrieved users',
+    USERS_RETRIEVING_ERROR = 'Error retrieved users',
 
     USER_CREATE_INITIATED = 'User create initiated',
     USER_CREATED_SUCCESS = 'User created successfully',
     USER_CREATED_CONFLICT = 'User created conflict',
-    USER_CREATED_FAILED = 'User created failed',
+    USER_CREATED_ERROR = 'User created failed',
+
+    USER_UPDATE_INITIATED = 'User update initiated',
+    USER_UPDATED_SUCCESS = 'User updated successfully',
+    USER_UPDATED_NOT_FOUND = 'User updated not found',
+    USER_UPDATED_FAILED = 'User updated failed',
+
+    USER_DELETED_INITIATED = 'User delete initiated',
+    USER_DELETED_SUCCESS = 'User deleted successfully',
+    USER_DELETED_NOT_FOUND = 'User deleted not found',
+    USER_DELETED_ERROR = 'User deleted failed',
 
 }
