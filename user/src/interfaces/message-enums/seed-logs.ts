cd47a997b897356a684da609979a1553ea156262
Oleh Strokan <oleh.strokan@enrian.com>
2023-06-16 10:09:44 +0200

    v2.6.6. Updated seed, created clear tables method

--- user/src/interfaces/message-enums/seed-logs.ts
+++ user/src/interfaces/message-enums/seed-logs.ts
@@ -1,5 +1,12 @@
 export enum SeedLogMessage {
+    DATABASE_SEEDING_INIT= 'Database seeding initiated.',
     DATABASE_SEEDING_COMPLETED = 'Database seeding completed.',
     DATABASE_ALREADY_CONTAINS_DATA = 'Database already contains data. Skipping seeding.',
     ERROR_SEEDING_DATABASE = 'Error seeding the database:',
+
+    USERS_CLEARING_INITIATED = 'Users table clearing initiated',
+    USERS_TABLE_CLEARED = 'Users table cleared successfully.',
+    ERROR_CLEARING_USERS_TABLE = 'Error clearing users table.',
+
+
 }
--- user/src/services/seed.service.ts
+++ user/src/services/seed.service.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 import {UserEntity} from "../repository/user.entity";
 import * as bcrypt from 'bcrypt';
 import {SeedLogMessage} from "../interfaces/message-enums/seed-logs";
+import {ActivationLinkEntity} from "../repository/activation-link.entity";
 
 @Injectable()
 export class SeedService {
@@ -14,21 +15,32 @@ export class SeedService {
 
     async seedDatabase(): Promise<void> {
         try {
+
+            this.logger.log(SeedLogMessage.DATABASE_SEEDING_INIT)
+
             const userRepository: Repository<UserEntity> = this.connection.getRepository(UserEntity);
+            const activationLinkRepository: Repository<ActivationLinkEntity> = this.connection.getRepository(ActivationLinkEntity);
 
             // Check if the tables already have data
             const hasData = await this.checkDataExistence(userRepository);
 
-
             if (!hasData) {
                 const testData = fs.readFileSync('/Users/stroka01/Development/medium-clone/user/test_db.json', 'utf-8');
 
-                const users = JSON.parse(testData);
+                const data = JSON.parse(testData);
 
-                for (const userData of users.users) {
+                for (const userData of data.users) {
                     const hashPassword = await bcrypt.hash(userData.password, 10);
-                    const user = userRepository.create({...userData, password: hashPassword})
+                    const user = await userRepository.create({...userData, password: hashPassword})
                     await userRepository.save(user);
+                }
+
+                for (const activationLinkData of data.activationLinks) {
+                    const activationLink = await activationLinkRepository.create({
+                        userId: activationLinkData.userId,
+                        link: activationLinkData.link
+                    })
+                    await activationLinkRepository.save(activationLink);
                 }
 
                 this.logger.log(SeedLogMessage.DATABASE_SEEDING_COMPLETED);
@@ -36,14 +48,30 @@ export class SeedService {
                 this.logger.log(SeedLogMessage.DATABASE_ALREADY_CONTAINS_DATA);
             }
         } catch (error) {
-            console.log(error)
+            this.logger.log(SeedLogMessage.ERROR_SEEDING_DATABASE)
+            await this.clearTables()
             this.logger.error(`${SeedLogMessage.ERROR_SEEDING_DATABASE} ${error}`);
         }
     }
 
     async checkDataExistence(userRepository: Repository<UserEntity>): Promise<boolean> {
-        // Check if the users table has any data
         const users = await userRepository.find();
-        return users.length > 2;
+        return users.length > 1;
+    }
+
+    async clearTables(): Promise<void> {
+        try {
+            this.logger.log(SeedLogMessage.USERS_CLEARING_INITIATED)
+            const userRepository: Repository<UserEntity> = this.connection.getRepository(UserEntity);
+            const activationLinkRepository: Repository<ActivationLinkEntity> = this.connection.getRepository(ActivationLinkEntity);
+
+            await activationLinkRepository.query('TRUNCATE TABLE users RESTART IDENTITY');
+            await userRepository.query('TRUNCATE TABLE users RESTART IDENTITY');
+
+            this.logger.log(SeedLogMessage.USERS_TABLE_CLEARED)
+
+        } catch (e) {
+            this.logger.error(SeedLogMessage.ERROR_CLEARING_USERS_TABLE)
+        }
     }
 }
