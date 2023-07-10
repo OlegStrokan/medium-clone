02dcd7014cddb667947db41bc45627fc6759968d
Oleh Strokan <oleh.strokan@enrian.com>
2023-07-10 17:24:53 +0200

    v3.4.4. Git commit test

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { LoginResponseDto } from "../interfaces/auth/response-dto/login.dto";
-import { LoginDto } from "../interfaces/auth/request-dto/login.dto";
-import { RegistrationResponseDto } from "../interfaces/auth/response-dto/registration";
-import { RegistrationDto } from "../interfaces/auth/request-dto/registration";
-import { LogoutDto } from "../interfaces/auth/request-dto/logout.dto";
-import { LogoutResponseDto } from "../interfaces/auth/response-dto/logout.dto";
 
 
 export const BASE_URL = 'http://localhost:8000/'
@@ -15,32 +9,6 @@ const baseQuery = fetchBaseQuery({
 
 export const api = createApi({
     baseQuery: baseQuery,
-    endpoints: (builder) => ({
-        login: builder.mutation<LoginResponseDto, LoginDto>({
-            query: (dto) => ({
-                url: 'auth/login',
-                method: 'POST',
-                body: dto,
-            }),
-        }),
-        register: builder.mutation<RegistrationResponseDto, RegistrationDto>({
-            query: (dto) => ({
-                url: 'auth/register',
-                method: 'POST',
-                body: dto,
-            }),
-        }),
-        logout: builder.mutation<LogoutResponseDto, LogoutDto>({
-            query: (dto) => ({
-                url: 'auth/logout',
-                method: 'POST',
-                body: dto
-            }),
-        }),
-    }),
+    endpoints: () => ({}),
 });
 
-
-
-
-export const { useLoginMutation, useRegisterMutation, useLogoutMutation } = api
